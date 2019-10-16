716f195b75210a4b68baeddfd7547cb1b632baba
melzreal <melissa.real@hotmail.com>
2019-10-16 17:36:29 +0100

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 	post '/api/login', to: 'api/sessions#create'
+	get  '/api/get_current_user', to: 'api/sessions#get_current_user'
 	
+
     delete '/api/logout', to: 'sessions#destroy'
 
 	namespace :api do
